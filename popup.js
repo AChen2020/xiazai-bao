5b8d2afa4e8b2fd936e8ade88a47a2d5a29ed205
AChen <achensoftware@gmail.com>
2021-03-06 10:12:27 +1000

    First cut-off

---
+++ popup.js
@@ -0,0 +1,42 @@
+const findLinks = () => {
+    //document.getElementById("links").innerHTML="hello"
+    const links = document.links;
+    var extractedLinks = "";
+
+    for (let link of links) {
+        console.log(link.href)
+        if (link.href.startsWith("https://")) {
+            extractedLinks += link.href + "<br/>"
+        }
+    }
+    //thunder e2d
+
+    if (extractedLinks === "") {                                    
+        extractedLinks = "No links";
+    }
+
+    let selectedLinks = document.createElement("div");
+    
+    selectedLinks.style.width = "600px";
+    selectedLinks.style.height = "300px";
+    selectedLinks.style.position = "fixed";
+    selectedLinks.style.top = "100px";
+    selectedLinks.style.left = "200px";
+    selectedLinks.style.background = "black";
+    selectedLinks.style.color = "white";
+    selectedLinks.style.overflowY = "scroll";
+
+    selectedLinks.innerHTML = extractedLinks;
+    
+    document.body.appendChild(selectedLinks);
+}
+
+//document.getElementById("links").innerHTML = extractedLinks;
+
+
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: findLinks
+      });
+});
