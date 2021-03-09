f4dde204feb09d29f39e36b42d9a196472ee0597
AChen <achensoftware@gmail.com>
2021-03-09 19:41:46 +1000

    Delete a selected image

---
+++ background.js
@@ -0,0 +1,42 @@
+
+
+chrome.runtime.onInstalled.addListener(() =>{
+
+    chrome.contextMenus.create({
+        "id": "delete",
+        "title": "Delete",
+        "type": 'normal',
+        "contexts": ['all']
+    });
+
+});
+
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    console.log("clicked...info: " + JSON.stringify(info));
+    console.log("clicked...tab:  " + JSON.stringify(tab));
+
+    chrome.tabs.executeScript(tab.id, {
+        code: `(${findElement})(${JSON.stringify(info)})`,
+        frameId: info.frameId,
+        matchAboutBlank: true,
+        runAt: 'document_start',
+    }, ([result] = []) => {
+        if (chrome.runtime.lastError) {
+            console.error('Error: ' + chrome.runtime.lastError.message);
+        } else {
+            console.log('Alt:', result);
+        }
+    });
+});
+
+function findElement({mediaType, srcUrl}) {
+    const tagName = mediaType === 'image' ? 'img' : mediaType;
+    console.log("clicked on " + mediaType + "|" + srcUrl)
+    for (const el of document.querySelectorAll(tagName)) {
+      if (el.src === srcUrl) {
+          el.remove();
+        return "deleted";
+      }
+    }
+  }
---
+++ contentScript.js
@@ -0,0 +1,4 @@
+// document.addEventListener('contextmenu', e => {
+//     console.log("clicked: " + e.target);
+//     e.target.remove();
+// });
